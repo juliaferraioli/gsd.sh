b857000557e6953339d7c644e9e1d45a94336013
Julia Ferraioli <julia.ferraioli@gmail.com>
2013-02-09 16:31:26 -0800

    add levels to site muting

--- gsd.sh
+++ gsd.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 # edit this list, or set GSD_SITES to add your custom sites
-BASE_SITES="reddit.com forums.somethingawful.com somethingawful.com digg.com break.com news.ycombinator.com infoq.com bebo.com twitter.com facebook.com blip.com youtube.com vimeo.com delicious.com flickr.com friendster.com hi5.com linkedin.com livejournal.com meetup.com myspace.com plurk.com stickam.com stumbleupon.com yelp.com slashdot.com"
+
+CODER_SITES="reddit.com digg.com facebook.com youtube.com flickr.com vimeo.com stumbleupon.com myspace.com yelp.com"
+
+HACKER_SITES=$CODER_SITES" techcrunch.com techdirt.com news.ycombinator.com meetup.com linkedin.com slashdot.org geekwire.com news.google.com"
+
+ARCHITECT_SITES=$HACKER_SITES" twitter.com plus.google.com wikipedia.org pandora.com rdio.com spotify.com grooveshark.com"
+
+BLACKOUT_SITES=$ARCHITECT_SITES" github.com"
 
 SITES="$GSD_SITES $BASE_SITES"
 
@@ -16,15 +23,44 @@ fi
 # clean up previous entries from /etc/hosts
 sed -i -e '/#gsd$/d' $HOSTFILE
 
-# write hosts file if 'work' mode
-if [ "$1" != "--play" ]
-then
-    for SITE in $SITES
-    do
-	echo -e "127.0.0.1\t$SITE\t#gsd" >> $HOSTFILE
-	echo -e "127.0.0.1\twww.$SITE\t#gsd" >> $HOSTFILE
-    done
-    echo "work mode enabled, run with --play to disable"
+if [ "$1" = "--coder" ]
+  then
+  for SITE in $CODER_SITES
+  do
+    echo -e "127.0.0.1\t$SITE\t#gsd" >> $HOSTFILE
+    echo -e "127.0.0.1\twww.$SITE\t#gsd" >> $HOSTFILE 
+  done
+  echo "coder mode enabled, run with --play to disable"
+fi
+
+if [ "$1" = "--hacker" ]
+  then
+  for SITE in $HACKER_SITES
+  do
+    echo -e "127.0.0.1\t$SITE\t#gsd" >> $HOSTFILE
+    echo -e "127.0.0.1\twww.$SITE\t#gsd" >> $HOSTFILE 
+  done
+  echo "hacker mode enabled, run with --play to disable"
+fi
+
+if [ "$1" = "--architect" ]
+  then
+  for SITE in $ARCHITECT_SITES
+  do
+    echo -e "127.0.0.1\t$SITE\t#gsd" >> $HOSTFILE
+    echo -e "127.0.0.1\twww.$SITE\t#gsd" >> $HOSTFILE 
+  done
+  echo "architect mode enabled, run with --play to disable"
+fi
+
+if [ "$1" = "--blackout" ]
+  then
+  for SITE in $BLACKOUT_SITES
+  do
+    echo -e "127.0.0.1\t$SITE\t#gsd" >> $HOSTFILE
+    echo -e "127.0.0.1\twww.$SITE\t#gsd" >> $HOSTFILE 
+  done
+  echo "architect mode enabled, run with --play to disable"
 fi
 
 if [ -z "$GSD_RESET" ]
